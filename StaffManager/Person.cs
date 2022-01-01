ade4029f4e625975bec6af7a31a069f7a21881e9
Jan Urbaś <janurbas39@gmail.com>
2022-01-01 23:35:53 +0100

    Implement base class Person

---
+++ StaffManager/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaffManager
+{
+    internal abstract class Person
+    {
+        protected Person(string firstName, string lastName, DateTime birthDate)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            BirthDate = birthDate;
+        }
+        public string FirstName { get; }
+        public string LastName { get; }
+        public DateTime BirthDate { get; }
+    }
+}
