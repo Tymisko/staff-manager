41a1ee2209859e5551175239bd58f3faa12ccde5
Jan Urbaś <janurbas39@gmail.com>
2022-01-08 20:50:14 +0100

    Arrange elements on main form

---
+++ StaffManager/Main.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace StaffManager
+{
+    partial class Main
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnAdd = new System.Windows.Forms.Button();
+            this.BtnDismiss = new System.Windows.Forms.Button();
+            this.BtnEdit = new System.Windows.Forms.Button();
+            this.dgvDiary = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDiary)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BtnAdd
+            // 
+            this.BtnAdd.Location = new System.Drawing.Point(12, 12);
+            this.BtnAdd.Name = "BtnAdd";
+            this.BtnAdd.Size = new System.Drawing.Size(98, 22);
+            this.BtnAdd.TabIndex = 0;
+            this.BtnAdd.Text = "Add";
+            this.BtnAdd.UseVisualStyleBackColor = true;
+            // 
+            // BtnDismiss
+            // 
+            this.BtnDismiss.Location = new System.Drawing.Point(220, 12);
+            this.BtnDismiss.Name = "BtnDismiss";
+            this.BtnDismiss.Size = new System.Drawing.Size(98, 22);
+            this.BtnDismiss.TabIndex = 1;
+            this.BtnDismiss.Text = "Dismiss";
+            this.BtnDismiss.UseVisualStyleBackColor = true;
+            // 
+            // BtnEdit
+            // 
+            this.BtnEdit.Location = new System.Drawing.Point(116, 12);
+            this.BtnEdit.Name = "BtnEdit";
+            this.BtnEdit.Size = new System.Drawing.Size(98, 22);
+            this.BtnEdit.TabIndex = 2;
+            this.BtnEdit.Text = "Edit";
+            this.BtnEdit.UseVisualStyleBackColor = true;
+            // 
+            // dgvDiary
+            // 
+            this.dgvDiary.AllowUserToDeleteRows = false;
+            this.dgvDiary.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvDiary.BackgroundColor = System.Drawing.Color.WhiteSmoke;
+            this.dgvDiary.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDiary.Location = new System.Drawing.Point(12, 40);
+            this.dgvDiary.Name = "dgvDiary";
+            this.dgvDiary.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.dgvDiary.RowTemplate.Height = 25;
+            this.dgvDiary.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvDiary.Size = new System.Drawing.Size(949, 481);
+            this.dgvDiary.TabIndex = 3;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(975, 538);
+            this.Controls.Add(this.dgvDiary);
+            this.Controls.Add(this.BtnEdit);
+            this.Controls.Add(this.BtnDismiss);
+            this.Controls.Add(this.BtnAdd);
+            this.Name = "Main";
+            this.Text = "Staff Manager";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDiary)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button BtnAdd;
+        private Button BtnDismiss;
+        private Button BtnEdit;
+        private DataGridView dgvDiary;
+    }
+}
