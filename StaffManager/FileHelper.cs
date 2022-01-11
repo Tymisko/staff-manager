caeabbbfc12dc05fe04a45b0fa8107070ee958ec
Jan Urbaś <janurbas39@gmail.com>
2022-01-11 22:31:28 +0100

    Create generic FileHelper class that contains serializing methods

---
+++ StaffManager/FileHelper.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+
+namespace StaffManager
+{
+    public class FileHelper<T> where T : new()
+    {
+        private readonly string _filePath;
+
+        public FileHelper(string filePath)
+        {
+            _filePath = filePath;
+        }
+
+        public void SerializeToJson(T data)
+        {
+            File.WriteAllText(Program.StaffDiaryPath, JsonConvert.SerializeObject(data));
+        }
+
+        public T DeserializeFromJson()
+        {
+            if (File.Exists(_filePath))
+                return JsonConvert.DeserializeObject<T>(File.ReadAllText(_filePath));
+            else
+                return new T();
+        }
+    }
+}
--- StaffManager/Main.cs
+++ StaffManager/Main.cs
@@ -7,35 +7,13 @@ using System.Windows.Forms;
 namespace StaffManager
 {
     public partial class Main : Form
-    { 
+    {
+        private readonly FileHelper<List<Employee>> _fileHelper;
         public Main()
         {
             InitializeComponent();
-
-            var employees = new List<Employee>()
-            {
-                new Employee(0, "Jan", "Urbaœ", new DateTime(2001,04,19), new DateTime(2020, 12, 19), 2500),
-                new Employee(0, "Anna", "Moska³a", new DateTime(2002,07,20), new DateTime(2020, 12, 19), 3500),
-                new Employee(0, "Kamil", "Fudala", new DateTime(2001,01,12), new DateTime(2020, 12, 19), 1500),
-                new Employee(0, "Mariusz", "Gluza", new DateTime(2001,08,03), new DateTime(2022, 1, 1), 5500)
-            };
-
-            SerializeToFile(employees);
-            dgvDiary.DataSource = DeserializeFromFile();
-        }
-
-        private void SerializeToFile(List<Employee> employees)
-        {
-            File.WriteAllText(Program.StaffDiaryPath, JsonConvert.SerializeObject(employees));
-        }
-
-        private List<Employee> DeserializeFromFile()
-        {
-            if (File.Exists(Program.StaffDiaryPath))
-                return JsonConvert.DeserializeObject<List<Employee>>(File.ReadAllText(Program.StaffDiaryPath));
-            else
-                return new List<Employee>();
-        }
+            _fileHelper = new FileHelper<List<Employee>>(Program.StaffDiaryPath)
+        }        
 
         private void BtnAdd_Click(object sender, EventArgs e)
         {
